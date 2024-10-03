b3b75c28102f9c36733760349e291254d41207f7
Sergey Khromenkov <khromenkov00@bk.ru>
2024-10-03 09:33:17 +0200

    Initial commit

---
+++ HomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    Console.WriteLine("Введите значение t");
+    int t = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите значение x");
+    int x = int.Parse(Console.ReadLine());
+    double k = 7*Math.Pow(t,2)+3*Math.Sin(Math.Pow(x,3))+9.2;
+    Console.WriteLine($"Ответ число K = {k:F2}");
+}
+catch
+{
+}
